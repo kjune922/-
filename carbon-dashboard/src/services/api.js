980ac97d0dbe0582bcc3084252c94ed79b571354
wooyounglee <wooyounglee3312@naver.com>
2025-05-26 13:35:12 +0900

    cors_local->ec2

--- carbon-dashboard/src/services/api.js
+++ carbon-dashboard/src/services/api.js
@@ -1,25 +1,22 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
-
 export const fetchCarbonData = async () => {
-  const res = await axios.get(`${API_BASE}/api/carbon`);
+  const res = await axios.get('/api/carbon'); // ✅ 이렇게 절대 상대 경로!
   return res.data;
 };
 
-  export const fetchJobData = async () => {
-    const res = await axios.get(`${API_BASE}/api/jobs`);
-    return res.data;
-  };
+export const fetchJobData = async () => {
+  const res = await axios.get('/api/jobs');
+  return res.data;
+};
 
-  export const fetchClusterLoad = async () => {
-    const res = await axios.get(`${API_BASE}/api/cluster-load`);
-    return res.data;
-  };
+export const fetchClusterLoad = async () => {
+  const res = await axios.get('/api/cluster-load');
+  return res.data;
+};
 
-  export const fetchScheduleData = async () => {
-    const res = await axios.get(`${API_BASE}/api/schedule`);
-    return res.data;
-  };
-  
+export const fetchScheduleData = async () => {
+  const res = await axios.get('/api/schedule');
+  return res.data;
+};
